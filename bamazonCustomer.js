eacdb905be3dfea2716cba5557c13dcb26241c8e
Carolina RC <caro.rmz315@gmail.com>
2018-04-19 11:16:25 -0500

    added mysql and inquirer

---
+++ bamazonCustomer.js
@@ -0,0 +1,16 @@
+// download external node packages
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+// create the connection information for the sql database
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "iwtmbabp2018",
+  database: "bamazon"
+});
